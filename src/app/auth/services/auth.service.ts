be24bbf5d82e778226111cd401c19c68e5929cb1
josue173 <josue.perez04@outlook.com>
2025-01-13 11:39:45 -0600

    feat(auth): optimizando código

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -24,21 +24,19 @@ export class AuthService {
 
   constructor() {}
 
+  private setAuthUser(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
+
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        // console.log({ user, token });
-      }),
-      map(() => true),
-      catchError((err) => {
-        console.log(err);
-        return throwError(() => err.error.message);
-      })
+      map(({ user, token }) => this.setAuthUser(user, token)),
+      catchError((err) => throwError(() => err.error.message))
     );
   }
 
@@ -48,13 +46,7 @@ export class AuthService {
     if (!token) return of(false);
     const headers = new HttpHeaders().set('Authorization', `Bearer${token}`); // Obteniendo Headers
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ token, user }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        return true;
-      }),
-      // Error
+      map(({ user, token }) => this.setAuthUser(user, token)),
       catchError(() => {
         this._authStatus.set(AuthStatus.authenticated);
         return of(false);
