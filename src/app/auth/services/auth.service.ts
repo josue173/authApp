6b1030a561308e6f2d846506f3949a4e2b0d07cc
josue173 <josue.perez04@outlook.com>
2025-01-13 15:01:55 -0600

    fix(auth): fixing refresh dashboard page

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -23,34 +23,42 @@ export class AuthService {
   public authStatus = computed(() => this._authStatus()); // Señal de lectura
 
   constructor() {
-    this.chechAuthStatus().subscribe();
+    this.checkAuthStatus().subscribe();
   }
 
-  private setAuthUser(user: User, token: string): boolean {
+  private setAuthentication(user: User, token: string): boolean {
     this._currentUser.set(user);
     this._authStatus.set(AuthStatus.authenticated);
     localStorage.setItem('token', token);
+
     return true;
   }
 
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
+
     return this.http.post<LoginResponse>(url, body).pipe(
-      map(({ user, token }) => this.setAuthUser(user, token)),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError((err) => throwError(() => err.error.message))
     );
   }
 
-  chechAuthStatus(): Observable<boolean> {
-    const url: string = `${this.baseUrl}/auth/check-token`;
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
-    if (!token) return of(false);
-    const headers = new HttpHeaders().set('Authorization', `Bearer${token}`); // Obteniendo Headers
+
+    if (!token) {
+      // this.logout();
+      return of(false);
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ user, token }) => this.setAuthUser(user, token)),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError(() => {
-        this._authStatus.set(AuthStatus.authenticated);
+        this._authStatus.set(AuthStatus.notAuthenticated);
         return of(false);
       })
     );
